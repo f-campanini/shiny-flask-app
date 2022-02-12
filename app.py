bbf036b48c03b6fee043103b2e6e451e8f4ff1fb
f-campanini <francesco.campanini@datadoghq.com>
2022-02-12 15:40:17 +0000

    First commit: - loading port 4444 the page index.html displays a form to upload a file

---
+++ app.py
@@ -0,0 +1,22 @@
+from ddtrace import tracer
+from flask import Flask, render_template, request, redirect, url_for
+import logging
+import os
+
+app = Flask(__name__)
+
+@app.route('/')
+def index():
+    # logger.info("This log is from the index route Hello, world" + logger.name)
+    return render_template('index.html')
+
+@app.route('/', methods=['POST'])
+def upload_file():
+    uploaded_file = request.files['file']
+    if uploaded_file.filename != '':
+        uploaded_file.save(uploaded_file.filename)
+    return redirect(url_for('index'))
+
+if __name__ == '__main__':
+    app.run(host=os.getenv('IP', '0.0.0.0'), 
+            port=int(os.getenv('PORT', 4444)))
